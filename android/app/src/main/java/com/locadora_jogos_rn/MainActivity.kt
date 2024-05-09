bb271ee89c05c198f2ec8317f9e05c329401bfd4
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-05-09 15:01:32 -0300

    Initial commit
    Generated by react-native@0.74.1

---
+++ android/app/src/main/java/com/locadora_jogos_rn/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.locadora_jogos_rn
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "locadora_jogos_rn"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
