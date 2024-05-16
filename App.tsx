e1f9c53ef04a1b8746dd931d8060430ccc52f9b9
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-05-16 13:51:41 -0300

    update

--- App.tsx
+++ App.tsx
@@ -2,16 +2,11 @@ import React from "react";
 import { FlatList } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import {createStackNavigator } from "@react-navigation/stack";
-
-const Stack = createStackNavigator();
+import CadastroJogos from "./src/screens/CadastroJogos";
 
 function App(): React.JSX.Element {
   return(
-    <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name='CadastroJogos' component={} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <CadastroJogos />
   );
 }
 export default App;
--- src/screens/CadastroJogos.tsx
+++ src/screens/CadastroJogos.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Image, ImageBackground, ScrollView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 
 const CadastroJogos: React.FC = () => {
     const [nome, setNome] = useState<string>('');
@@ -11,7 +11,36 @@ const CadastroJogos: React.FC = () => {
     const [desenvolvedor, setDesenvolvedor] = useState<string>('');
     const [distribuidora, setDistribuidora] = useState<string>('');
     const [categoria, setCategoria] = useState<string>('');
-}
+
+    const cadastrarJogos = async () => {
+        try {
+            const formData = new FormData();
+            formData.append('nome', nome);
+            formData.append('preco', preco);
+            formData.append('descricao', descricao);
+            formData.append('classificacao', classificacao);
+            formData.append('plataformas', plataformas);
+            formData.append('desenvolvedor', desenvolvedor);
+            formData.append('distribuidora', distribuidora);
+            formData.append('categoria', categoria);
+
+            const response = await axios.post ('http://10.137.11.208:8000/api/register/games', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+        } catch(error){
+            console.log(error);
+        }
+    }
+    return (
+        <View>
+            <TouchableOpacity onPress={cadastrarJogos}>
+                <Text>Cadastrar Jogo</Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
         
 const styles = StyleSheet.create({
     container: {
