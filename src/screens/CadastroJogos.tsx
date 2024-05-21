1c3f0b31f708029a382fcaa6624da8cdc167d1d8
shiinzinho <gabrieldosantoslacerda@gmail.com>
2024-05-21 12:49:54 -0300

    update

--- src/screens/CadastroJogos.tsx
+++ src/screens/CadastroJogos.tsx
@@ -1,218 +1,263 @@
-import React, { useState } from "react";
-import {
-  Button,
-  Image,
-  ImageBackground,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from "react-native";
 import axios from "axios";
+import React, { useState } from "react";
+import { ScrollView, StatusBar, StyleSheet, TextInput, TouchableOpacity, } from "react-native";
+import { Image, View, Text } from "react-native-animatable";
+//npm install react-native-animatable
 
-const CadastroJogos: React.FC = () => {
-  const [nome, setNome] = useState<string>('');
-  const [preco, setPreco] = useState<string>('');
-  const [descricao, setDescricao] = useState<string>('');
-  const [classificacao, setClassificacao] = useState<string>('');
-  const [plataformas, setPlataformas] = useState<string>('');
-  const [desenvolvedor, setDesenvolvedor] = useState<string>('');
-  const [distribuidora, setDistribuidora] = useState<string>('');
-  const [categoria, setCategoria] = useState<string>('');
+function CadastroJogos(): React.JSX.Element {
+  const [jogos, setJogos] = useState<Jogos[]>([]);
+  const [nome, setNome] = useState<string>("");
+  const [preco, setPreco] = useState<string>("");
+  const [descricao, setDescricao] = useState<string>("");
+  const [classificacao, setClassificacao] = useState<string>("");
+  const [plataformas, setPlataformas] = useState<string>("");
+  const [desenvolvedor, setDesenvolvedor] = useState<string>("");
+  const [distribuidora, setDistribuidora] = useState<string>("");
+  const [categoria, setCategoria] = useState<string>("");
+  const [errors, setErrors] = useState<string>("");
+
+  const validateForm = () => {
+    const newErrors: any = {};
+
+    if (!nome) {
+      newErrors.nome = "O campo nome é obrigatório";
+    }
+
+    if (!preco) {
+      newErrors.preco = "O campo preço é obrigatório";
+    }
+
+    if (!descricao) {
+      newErrors.descricao = "O campo descrição é obrigatório";
+    }
+
+    if (!classificacao) {
+      newErrors.classificacao = "O campo classificação é obrigatório";
+    }
+
+    if (!plataformas) {
+      newErrors.plataformas = "O campo plataformas é obrigatório";
+    }
+
+    if (!desenvolvedor) {
+      newErrors.desenvolvedor = "O campo desenvolvedor é obrigatório";
+    }
+
+    if (!distribuidora) {
+      newErrors.distribuidora = "O campo distribuidora é obrigatório";
+    }
+
+    setErrors(newErrors);
+
+    return !Object.keys(newErrors).length;
+  };
+
 
   const cadastrarJogos = async () => {
-    try {
-      const formData = new FormData();
-      formData.append('nome', nome);
-      formData.append('preco', preco);
-      formData.append('descricao', descricao);
-      formData.append('classificacao', classificacao);
-      formData.append('plataformas', plataformas);
-      formData.append('desenvolvedor', desenvolvedor);
-      formData.append('distribuidora', distribuidora);
-      formData.append('categoria', categoria);
+    if (validateForm()) {
+      try {
 
-      const response = await axios.post('http://10.137.11.206:8000/api/register/games', formData, {
-        headers: {
-          'Content-Type': 'ultipart/form-data'
+        const formData = new FormData();
+        formData.append('nome', nome);
+        formData.append('preco', preco);
+        formData.append('descricao', descricao);
+        formData.append('classificacao', classificacao);
+        formData.append('plataformas', plataformas);
+        formData.append('desenvolvedor', desenvolvedor);
+        formData.append('distribuidora', distribuidora);
+        formData.append('categoria', categoria);
+
+        const response = await axios.post('http://10.137.11.206:8000/api/register/games', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        console.log(response)
+      } catch (error) {
+        if (error.response && error.response.data && error.response.data.errors) {
+          setErrors(error.response.data.errors);
+        } else {
+          console.log(error);
         }
-      });
-    } catch (error) {
-      console.log(error);
+      }
     }
+
   }
 
   return (
-    <ImageBackground source={require('../assets/images/singup.jpg')} style={styles.imageBackground}>
-      <View style={styles.container}>
+    <View style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <StatusBar backgroundColor={'white'}></StatusBar>
         <View style={styles.header}>
-          <Text style={styles.headerText}>Cadastro de Jogos</Text>
+          <Image style={styles.imagem} source={require('../assets/images/logo.png')}></Image>
         </View>
-        <TextInput
-          style={styles.input}
-          placeholder="Nome do Produto"
-          value={nome}
-          onChangeText={setNome}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Preço"
-          value={preco}
-          onChangeText={setPreco}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Descrição"
-          value={descricao}
-          onChangeText={setDescricao}
-          color='#010E2C'
-          multiline
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Classificação"
-          value={classificacao}
-          onChangeText={setClassificacao}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Plataformas"
-          value={plataformas}
-          onChangeText={setPlataformas}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Desenvolvedor"
-          value={desenvolvedor}
-          onChangeText={setDesenvolvedor}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Distribuidora"
-          value={distribuidora}
-          onChangeText={setDistribuidora}
-          color='#010E2C'
-        />
-        <TextInput
-          style={styles.input}
-          placeholder="Categoria"
-          value={categoria}
-          onChangeText={setCategoria}
-          color='#010E2C'
-        />
-        <Button title="Cadastrar" onPress={cadastrarJogos} />
-      </View>
-    </ImageBackground>
+
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Nome:"
+            placeholderTextColor={'white'}
+            value={nome}
+            onChangeText={setNome}
+          />
+          {errors.nome && <Text style={styles.errorText}>{errors.nome}</Text>}
+
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Preço:"
+            placeholderTextColor={'white'}
+            value={preco}
+            onChangeText={setPreco}
+          />
+          {errors.preco && <Text style={styles.errorText}>{errors.preco}</Text>}
+
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Descrição:"
+            placeholderTextColor={'white'}
+            value={descricao}
+            onChangeText={setDescricao}
+          />
+          {errors.descricao && <Text style={styles.errorText}>{errors.descricao}</Text>}
+
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Classificação:"
+            placeholderTextColor={'white'}
+            value={classificacao}
+            onChangeText={setClassificacao}
+          />
+          {errors.classificacao && <Text style={styles.errorText}>{errors.classificacao}</Text>}
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Plataformas:"
+            placeholderTextColor={'white'}
+            value={plataformas}
+            onChangeText={setPlataformas}
+          />
+          {errors.plataformas && <Text style={styles.errorText}>{errors.plataformas}</Text>}
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Desenvolvedor:"
+            placeholderTextColor={'white'}
+            value={desenvolvedor}
+            onChangeText={setDesenvolvedor}
+          />
+          {errors.desenvolvedor && <Text style={styles.errorText}>{errors.desenvolvedor}</Text>}
+
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Distribuidora:"
+            placeholderTextColor={'white'}
+            value={distribuidora}
+            onChangeText={setDistribuidora}
+          />
+          {errors.distribuidora && <Text style={styles.errorText}>{errors.distribuidora}</Text>}
+
+        </View>
+        <View style={styles.form}>
+          <TextInput style={styles.input}
+            placeholder="Categoria:"
+            placeholderTextColor={'white'}
+            value={categoria}
+            onChangeText={setCategoria}
+          />
+          {errors.categoria && <Text style={styles.errorText}>{errors.categoria}</Text>}
+
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={cadastrarJogos}>
+          <Text style={styles.buttonText}>Cadastrar</Text>
+        </TouchableOpacity>
+        <View style={styles.footer}>
+
+        </View>
+      </ScrollView>
+    </View>
+
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#301461'
   },
   header: {
-    height: 55,
-    marginBottom: 200,
-    backgroundColor: '#000',
-    justifyContent: 'space-around',
+    backgroundColor: '',
     alignItems: 'center',
-    paddingHorizontal: 60,
-    borderBottomStartRadius: 22,
-    borderBottomEndRadius: 22
-  },
-  errorText: {
-    color: 'yellow',
-    marginBottom: 5,
-  },
-  headerText: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
-  form: {
-    borderRadius: 15,
-    padding: 10,
-    backgroundColor: 'black',
-    marginBottom: 10,
-  },
-  input: {
-    height: 40,
-    width: 350,
-    borderColor: 'white',
-    borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    borderRadius: 10,
-    color: 'black',
-    fontWeight: 'bold',
-    backgroundColor: 'white',
-  },
-  imageButton: {
-    backgroundColor: 'red',
-    padding: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-    marginBottom: 10,
-  },
-  imageButtonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  fotoSelecionada: {
-    width: 200,
-    height: 200,
-    resizeMode: 'cover',
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  alinhamentofotoSelecionada: {
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: 'red',
-    padding: 10,
-    borderRadius: 10,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-  },
-  imageBackground: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  imageHeader: {
-    width: 320,
-    height: 150,
+    paddingVertical: 100,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 100,
+    height: 10,
+    marginTop: -35
   },
   footer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'black',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+    paddingVertical: 50,
+    backgroundColor: '#151f42',
+    marginTop: 20,
     alignItems: 'center',
-    paddingVertical: 10,
-    borderTopWidth: 3,
-    borderColor: 'red',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+
   },
-  footerIcon: {
-    width: 30,
-    height: 30
+  form: {
+    width: 360,
+    marginLeft: 10,
+    marginTop: -25,
+
   },
-});
+  input: {
+    backgroundColor: 'transparent',
+    marginTop: 32,
+    fontWeight: 'bold',
+    height: 50,
+    borderBottomWidth: 2,
+    borderColor: '#fff',
+    color: 'white',
+    paddingLeft: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imagem: {
+    height: 100,
+    width: 340,
+    marginTop: -30,
+    marginRight: 15,
+    borderWidth: 5,
+  },
+  button: {
+    backgroundColor: "white",
+    borderRadius: 25,
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    marginTop: 20,
+    height: 50,
+    width: 250,
+    marginLeft: 65,
+  },
+  buttonText: {
+    color: "#2E8B57",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  imageButtonText: {
+    color: '#3CB371',
+    fontWeight: 'bold',
+    marginTop: -40,
+    marginLeft: 125
+  },
+  errorText: {
+    fontWeight: 'bold',
+    marginLeft: 10,
+    color: 'white'
+  },
+
+})
 
 export default CadastroJogos;
